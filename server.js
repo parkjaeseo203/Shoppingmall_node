e01fe2214328023b536f99703f9612490ca6d266
parkjaeseo <qkrwotj203@gmail.com>
2020-08-18 19:28:16 +0900

    add morgan

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 
-// app.use((req, res) => {
-//     res.json({
-//         message: 'It works'
-//     })
-// })
 const productRoute = require('./routes/product')
 const orderRoute = require('./routes/order')
 
+// 미들웨어 설정
+app.use(morgan('dev'))
 
+
+
+
+
+// 라우팅
 app.use('/product', productRoute)
 app.use('/order', orderRoute)
 
