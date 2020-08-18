5297caea9ab3c497dbd356b75659391ed4985bb8
parkjaeseo <qkrwotj203@gmail.com>
2020-08-18 18:23:42 +0900

    response of using app

--- server.js
+++ server.js
@@ -3,7 +3,18 @@ const express = require('express')
 const app = express()
 
 
+app.use((req, res) => {
+    res.json({
+        message: 'It works'
+    })
+})
+
+
+
+
 
 const port = 4275
 
 app.listen(port, console.log('server started'))
+
+
