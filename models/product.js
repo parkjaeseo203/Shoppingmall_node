9f61d25845df3507caa376bdb12f9a4a54dca507
parkjaeseo <qkrwotj203@gmail.com>
2020-08-23 15:47:36 +0900

    Product model

---
+++ models/product.js
@@ -0,0 +1,17 @@
+
+//1
+const mongoose = require('mongoose')
+
+//2
+const productSchema = mongoose.Schema({
+    //4
+    name: String,
+    price: Number
+})
+
+
+
+
+
+//3
+module.exports = mongoose.model("product", productSchema)
