5e0eff84ce69b920ea479af42659478c63f4833d
parkjaeseo <qkrwotj203@gmail.com>
2020-09-03 12:02:07 +0900

    email register

--- routes/user.js
+++ routes/user.js
@@ -10,38 +10,62 @@ const userModel = require('../models/user')
 // 회원가입
 router.post('/register', (req, res) => {
 
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
 
-        if(err) {
-            return res.json({
-                error: err.message
-            })
-        }
+    // email check -> password 암호화 -> model/user db저장
 
-        else{
-
-            const newUser = new userModel({
-                name: req.body.name,
-                email: req.body.email,
-                password: hash
-            })
-
-            newUser
-                .save()
-                .then(user => {
-                    res.json({
-                        message: 'completly joined',
-                        userInfo: user
-                    })
+    userModel
+        .findOne({email: req.body.email})
+        .then(user => {
+            if (user) {
+                return res.json({
+                    message: 'email already exists'
                 })
-                .catch(err => {
-                    res.json({
-                        message: err.message
-                    })
+            }
+
+            else {
+
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+
+                    if(err) {
+                        return res.json({
+                            error: err.message
+                        })
+                    }
+
+                    else{
+
+                        const newUser = new userModel({
+                            name: req.body.name,
+                            email: req.body.email,
+                            password: hash
+                        })
+
+                        newUser
+                            .save()
+                            .then(user => {
+                                res.json({
+                                    message: 'completly joined',
+                                    userInfo: user
+                                })
+                            })
+                            .catch(err => {
+                                res.json({
+                                    message: err.message
+                                })
+                            })
+
+                    }
                 })
 
-        }
-    })
+            }
+        })
+        .catch(err => {
+            res.json({
+                message: err.message
+            })
+        })
+
+
 
 
 
