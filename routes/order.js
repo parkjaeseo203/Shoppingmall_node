f67af7fc1d7d030e7a9243d91af8ef9eeff5a64d
parkjaeseo <qkrwotj203@gmail.com>
2020-09-03 11:00:16 +0900

    order resister total/detail get

--- routes/order.js
+++ routes/order.js
@@ -2,21 +2,90 @@
 const express = require('express')
 const router = express.Router()
 
+const orderModel = require('../models/order')
 //order 불러오기
 
 router.get('/', (req, res) => {
-    res.json({
-        message: 'order 불러오기'
-    })
+
+    orderModel
+        .find()
+        .populate("product", ["name", "price"])
+        .then(docs => {
+            res.json({
+                message: 'Successfully get your Items',
+                count: docs.length,
+                products: docs.map(doc => {
+                    return{
+                        id: doc._id,
+                        product: doc.product,
+                        quantity: doc.quantity
+                    }
+                })
+            })
+        })
+        .catch(err => {
+            res.json({
+                message: err.message
+            })
+        })
+    // res.json({
+    //     message: 'order 불러오기'
+    // })
 })
 
+router.get('/:productID', (req, res) => {
+    orderModel
+        .findById(req.params.productID)
+        .populate("product", ["name", "price"])
+        .then(doc => {
+            res.json({
+                message: 'Successfully take a ' + req.params.productID,
+                productInfo: {
+                    id: doc._id,
+                    product: doc.product,
+                    quantity: doc.quantity
+
+                }
+            })
+        })
+        .catch(err => {
+            res.json({
+                message: err.message
+            })
+        })
+})
 
 //order 등록하기
 
 router.post('/', (req, res) => {
-    res.json({
-        message: 'order 등록하기'
+
+    const newOrder = new orderModel({
+        product: req.body.productID,
+        quantity: req.body.qty
     })
+
+    newOrder
+        .save()
+        .then(doc => {
+            res.json({
+                message: 'Order saved',
+                orderInfo: {
+                    id: doc._id,
+                    product: doc.product,
+                    quantity: doc.quantity
+                }
+            })
+        })
+        .catch(err => {
+            res.json({
+                message: err.message
+            })
+        })
+
+
+    // res.json({
+    //     message: 'order 등록하기'
+    // })
 })
 
 
